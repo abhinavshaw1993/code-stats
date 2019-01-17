4c49d12c783f3aa8a917e2678642c6e1eb4a23fe
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2019-01-17 07:35:55 -0500

    Feature for showing Language wise lines of code.

--- code_evaluator/bin/statistics.py
+++ code_evaluator/bin/statistics.py
@@ -11,13 +11,31 @@ from collections import Counter
 from pathlib import Path
 from code_evaluator.utils.logging_utils import Logger
 
-kDefaultFileExtYamlFilePath = "data/default_file_ext_list.yaml"
+kDefaultFileExtYamlFilePath = "data/default_ext_list.yml"
 
-def get_file_extension_list(file_ext_list_file_path):
+def get_extension_dict(file_ext_list_file_path):
+    language_dict = data_utils.read_yaml(file_ext_list_file_path)
+
+    return language_dict
+
+def get_file_extension_list_from_dict(extensions_dict):
     """
     Return file extension list
     """
-    return data_utils.read_yaml(file_ext_list_file_path)['file_ext']
+    extension_list = []
+    for e in extensions_dict.values():
+        extension_list += e
+
+    return extension_list
+
+def get_ext_to_language_map_from_dict(extensions_dict):
+    ext_to_language_map = {}
+    for e in extensions_dict.keys():
+        values = extensions_dict[e]
+        for v in values:
+            ext_to_language_map[v] = e
+
+    return ext_to_language_map
 
 def evaluate_lines_of_code(path_to_project,
                            ignore_blank_lines=False,
@@ -32,16 +50,17 @@ def evaluate_lines_of_code(path_to_project,
 
     files = utils.get_files_in_dir(path_to_project)
     folders = utils.get_folders_in_dir(path_to_project)
-    file_ext_list = get_file_extension_list(file_ext_list_file_path)
-
+    ext_dict = get_extension_dict(file_ext_list_file_path)
+    ext_list = get_file_extension_list_from_dict(ext_dict)
+    ext_lang_map = get_ext_to_language_map_from_dict(ext_dict)
     # Loop to count lines of files.
     for file in files:
         file_name, file_ext = os.path.splitext(file)
 
-        if file_ext in file_ext_list :
+        if file_ext in ext_list :
             Logger().verbose_print("File: ", Path(file))
             line_count = utils.get_total_line_for_file(file)
-            lines_of_code_per_language[file_ext] += line_count
+            lines_of_code_per_language[ext_lang_map[file_ext]] += line_count
             lines_of_code += line_count
 
     return lines_of_code, lines_of_code_per_language
